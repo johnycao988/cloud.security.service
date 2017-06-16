95f62d8d57ef1bbc335cea6d997cc9270fb4b08f
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-06-16 15:26:43 +0800

    AA

--- src/main/java/com/cly/cloud/security/service/app/UserRest.java
+++ src/main/java/com/cly/cloud/security/service/app/UserRest.java
@@ -3,14 +3,11 @@ package com.cly.cloud.security.service.app;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.ui.ModelMap;
+import javax.servlet.http.HttpServletResponse; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController; 
 
 import com.cly.comm.client.http.HttpRequestParam;
 import com.cly.security.SecuConst;
